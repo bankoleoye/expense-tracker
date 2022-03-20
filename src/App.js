d5350bc9e3ed8c5ec12fa7d9e3f140fb74baac3b
Bankole Oyewole <66924805+bankoleoye@users.noreply.github.com>
2022-03-20 23:56:44 +0100

    created props and passed the data through props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "100GB",
+      amount: 40000,
+      date: new Date(2021, 11, 9),
+    },
+    {
+      id: "e2",
+      title: "80GB",
+      amount: 20000,
+      date: new Date(2021, 3, 12),
+    },
+    {
+      id: "e3",
+      title: "50GB",
+      amount: 15000,
+      date: new Date(2022, 1, 19),
+    },
+    {
+      id: "e3",
+      title: "90GB",
+      amount: 35000,
+      date: new Date(2022, 2, 22),
+    },
+  ];
+
   return (
     <div>
       <h2>Lets's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(19, 3, 2021);
-  const expenseAmount = 16000;
-  const expenseTitle = "Monthly Data Subscription";
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">#{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">#{props.amount}</div>
       </div>
     </div>
   );
